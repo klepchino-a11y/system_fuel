30c5616eb7c9071af35bfefc069794ab2e98f4d5
klepchino-a11y <klepchino@gmail.com>
2025-10-22 00:08:05 +0300

    Add files via upload

---
+++ system_fuel/html/translations.js
@@ -0,0 +1,30 @@
+export const TRANSLATIONS = {
+    en: {
+        fuel: "Fuel",
+        vehicle_fuel: "Your vehicle has %{capacity} liters of fuel.",
+        liters: "Liters: ",
+        price: "Price: $",
+        refill: "Refill",
+    },
+    es: {
+        fuel: "Combustible",
+        vehicle_fuel: "Tu vehículo tiene %{capacity} litros de combustible.",
+        liters: "Litros: ",
+        price: "Precio: $",
+        refill: "Recargar",
+    },
+    nl: {
+        fuel: "Brandstof",
+        vehicle_fuel: "Je voertuig heeft %{capacity} liter brandstof.",
+        liters: "Liters: ",
+        price: "Prijs: $",
+        refill: "Bijvullen",
+    },
+  ar: {
+    fuel: "الوقود",
+    vehicle_fuel: "مركبتك تحتوي على %{capacity} لترًا من الوقود.",
+    liters: "اللترات: ",
+    price: "السعر: $",
+    refill: "تعبئة",
+  },
+};
