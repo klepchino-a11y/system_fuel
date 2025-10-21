c6df44e54e64bc807915e885d26e568080ec686b
klepchino-a11y <klepchino@gmail.com>
2025-10-21 23:36:55 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,40 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Cocodrulo (base) + custom edits'
+version '0.0.5'
+description 'Fuel System (ESX + ox_target) with per-vehicle config and HUD'
+
+ui_page 'html/index.html'
+
+shared_scripts {
+    '@es_extended/imports.lua',
+    'shared/locale.lua',
+    'locales/*.lua',
+    'Config.lua',
+}
+
+client_scripts {
+    'client/main.lua',
+    'client/hud_and_consumption.lua', -- إضافة HUD + استهلاك
+}
+
+server_scripts {
+    'server/main.lua',
+}
+
+files {
+    'html/index.html',
+    'html/app.js',          -- الملف الموجود فعليًا داخل المورد
+    'html/translations.js',
+    'html/style.css',
+}
+
+dependency 'ox_target'
+provide 'LegacyFuel'
+
+-- تصدير دوال للوصول لمستوى الوقود (عميل)
+exports {
+    'GetFuel',
+    'SetFuel',
+}
